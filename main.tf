66183c8f7584bff23f62671624f6d61a6a92d935
Tejaswi <34575326+tejuteju@users.noreply.github.com>
2022-08-29 15:11:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "google_data_fusion_instance" "data-fusion-private-instance" {
     network                     = google_compute_network.custom-test.name
     ip_allocation               = var.private_ip_range
   }
+  version=var.df_version
   labels = {
     example_key = var.label_value
   } 
